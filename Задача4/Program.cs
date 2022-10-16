a321291be0b73e09a365bfb21ad315093ecbd5b1
Alexey <brazhnikovas_87@bk.ru>
2022-10-16 16:50:10 +0500

    готовая

---
+++ Задача4/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 4: Напишите программу, 
+//которая принимает на вход три числа и 
+Console.WriteLine("Программа для поиска максимального числа из тех чисел");
+Console.WriteLine("Впишите первое число");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Впишите второе число");
+int numberB = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Впишите третье число");
+int numberC = int.Parse(Console.ReadLine()!);
+int max = 0;
+//выдаёт максимальное из этих чисел
+if(numberA>numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if(max>numberC)
+{
+    Console.WriteLine("Максимальное число");
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine("Максимальное число");
+    Console.WriteLine(numberC);
+}
+
+    
