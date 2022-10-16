4dbc2023595f4ec7a9da5fb5e6703bcbf59d76fe
Alexey <brazhnikovas_87@bk.ru>
2022-10-16 15:59:02 +0500

    итог

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Впишите 2 числа");
+﻿// Описание
+Console.WriteLine("Программа для поиска большего числа и двух");
+// Ввод чисел
+Console.WriteLine("Впишите первое число");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Впишите второе число");
+int numberB = int.Parse(Console.ReadLine()!);
+// Выбираем большее и ыводим в консоль
+if(numberA>numberB)
+{
+    Console.WriteLine("Число");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Больше");
+}
+else
+{
+    Console.WriteLine("Число");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Больше");
+}
+
+
